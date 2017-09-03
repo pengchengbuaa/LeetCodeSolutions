b1d88ebca4f211e2fb772aae7a690eb298e32528
Peng Cheng <pengcheng@pengcheng-01.local>
2017-09-04 00:27:09 +0800

    add reference answer

--- 16.3SumClosest.cpp
+++ 16.3SumClosest.cpp
@@ -1,6 +1,41 @@
 #include<vector>
 using namespace std;
 
+
+
+/*
+int threeSumClosest(vector<int> &num, int target) {        
+    vector<int> v(num.begin(), num.end()); // I didn't wanted to disturb original array.
+    int n = 0;
+    int ans = 0;
+    int sum;
+    
+    sort(v.begin(), v.end());
+    
+    // If less then 3 elements then return their sum
+    while (v.size() <= 3) {
+        return accumulate(v.begin(), v.end(), 0);
+    }
+    
+    n = v.size();
+    
+    ans = v[0] + v[1] + v[2];
+    for (int i = 0; i < n-2; i++) {
+        int j = i + 1;
+        int k = n - 1;
+        while (j < k) {
+            sum = v[i] + v[j] + v[k];
+            if (abs(target - ans) > abs(target - sum)) {
+                ans = sum;
+                if (ans == target) return ans;
+            }
+            (sum > target) ? k-- : j++;
+        }
+    }
+    return ans;
+}
+
+*/
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
@@ -8,8 +43,7 @@ public:
 	int n = nums.size();
 	int closestGap = INT_MAX;
 	int sum;
-
-
+	
 	int b,c;
 	int gap = INT_MAX;
 	for(int a=0 ;a <n-2; a++){
