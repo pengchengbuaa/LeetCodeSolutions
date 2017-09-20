b76b215dfc30103a685cde2552fe500deb72248a
pengcheng <pengcheng@megvii.com>
2017-09-20 14:23:45 +0800

    doc 44

--- 44.WildcardMatching.cpp
+++ 44.WildcardMatching.cpp
@@ -22,10 +22,31 @@ class Solution {
 	}
 };
 
+//http://yucoding.blogspot.com/2013/02/leetcode-question-123-wildcard-matching.html
+/*
+Analysis:
 
+For each element in s
+If *s==*p or *p == ? which means this is a match, then goes to next element s++ p++.
+If p=='*', this is also a match, but one or many chars may be available, so let us save this *'s position and the matched s position.
+If not match, then we check if there is a * previously showed up,
+       if there is no *,  return false;
+       if there is an *,  we set current p to the next element of *, and set current s to the next saved s position.
+
+e.g.
+
+abed
+?b*d**
+
+a=?, go on, b=b, go on,
+e=*, save * position star=3, save s position ss = 3, p++
+e!=d,  check if there was a *, yes, ss++, s=ss; p=star+1
+d=d, go on, meet the end.
+check the rest element in p, if all are *, true, else false;
+
+Note that in char array, the last is NOT NULL, to check the end, use  "*p"  or "*p=='\0'".
 //Linearal solution 
 //
-/*
  *  bool isMatch(const char *s, const char *p) {
         const char* star=NULL;
         const char* ss=s;
