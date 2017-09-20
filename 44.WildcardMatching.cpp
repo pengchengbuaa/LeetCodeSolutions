29f396c427761b7a97cf1c50416fc3c7d0cee348
pengcheng <pengcheng@megvii.com>
2017-09-20 14:18:45 +0800

    doc 44

--- 44.WildcardMatching.cpp
+++ 44.WildcardMatching.cpp
@@ -10,7 +10,6 @@ class Solution {
 	    int lp=p.size();
 	    vector<vector<int> > vv(lp+1,vector<int>(ls+1,0));
 	    vv[0][0]=1;
-
 	    for(int i=1;i<=lp;i++){
 		for(int j=0;j<=ls;j++){
 			//last term vv[i-1][j]==1 : a , a*  match !
@@ -23,3 +22,33 @@ class Solution {
 	}
 };
 
+
+//Linearal solution 
+//
+/*
+ *  bool isMatch(const char *s, const char *p) {
+        const char* star=NULL;
+        const char* ss=s;
+        while (*s){
+            //advancing both pointers when (both characters match) or ('?' found in pattern)
+            //note that *p will not advance beyond its length
+            if ((*p=='?')||(*p==*s)){s++;p++;continue;}
+
+            // * found in pattern, track index of *, only advancing pattern pointer
+            if (*p=='*'){star=p++; ss=s;continue;}
+
+            //current characters didn't match, last pattern pointer was *, current pattern pointer is not *
+            //only advancing pattern pointer
+            if (star){ p = star+1; s=++ss;continue;}
+
+           //current pattern pointer is not star, last patter pointer was not *
+           //characters do not match
+            return false;
+        }
+
+       //check for remaining characters in pattern
+        while (*p=='*'){p++;}
+
+        return !*p;
+    }
+    */
