79d286d4292315551a19bda31be5a4b5f5ca1539
pengcheng <pengcheng@megvii.com>
2017-09-24 10:22:35 +0800

    add 51

---
+++ 51.N-Queens.cpp
@@ -0,0 +1,51 @@
+#include<string>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    string findvalid(vector<string> b,int k,int n){
+	string s(n,'.');
+	for(int i=0;i<k-1;i++){
+	    int j=0;
+	    while(true){
+		if (b[i][j]=='Q')   break;
+		j++;
+	    }
+	    s[j]='x';
+	    int j1=j+(k-1-i);
+	    int j2=j-(k-1-i);
+	    if(j1>=0 && j1<n)	s[j1]='x';
+	    if(j2>=0 && j2<n)	s[j2]='x';
+	}
+	return s;
+    }
+
+    void back(vector<string> & b,int k,int n,vector<vector<string>> &  ans){
+	if(k==n)    {
+	    ans.push_back(b);
+	    return;
+	}
+	else{
+	    string valid=findvalid(b,k+1,n);
+	    for(int i=0;i<n;i++){
+		if(valid[i]=='x')   continue;
+		b[k][i]='Q';
+		back(b,k+1,n,ans);
+		b[k][i]='.';
+	    }
+	}
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+	vector<vector<string>> ans;
+	vector< string> b(n,string(n,'.'));
+	for(int i=0;i<n;i++){
+	    string fir(n,'.');
+	    fir[i]='Q';
+	    b[0]=fir;
+	    back(b,1,n,ans);
+	}
+	return ans;
+    }
+};
